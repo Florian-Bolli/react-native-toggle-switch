d231ecd0658034a80686136ecc4efcaa8dc9cc11
Florian Bolli <mail@flroianbolli.ch>
2020-11-28 14:47:47 +0100

    biggerIndeicator

--- scrollview-switch.js
+++ scrollview-switch.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   ScrollView,
   View,
@@ -6,25 +6,31 @@ import {
   StyleSheet,
   TouchableWithoutFeedback,
   TouchableOpacity,
-  Platform
-} from 'react-native';
+  Platform,
+} from "react-native";
 
 class ToggleSwitch extends React.Component {
   constructor(...props) {
     super(...props);
     this.state = {
-      isActive: this.props.active || false
+      isActive: this.props.active || false,
     };
     this.borderWidth = 2;
     this.universalPadding = 2;
-    this.viewPortRadius =  this.props.radius +  this.universalPadding;
+    this.viewPortRadius = this.props.radius + this.universalPadding;
     this.textMargin = 8;
-    this.viewPortWidth = this.props.width + (2 * this.props.radius) + (2 * this.universalPadding) + this.textMargin;
-    this.initailContentOffset = this.props.active ? 0 : this.props.width  + this.textMargin;
+    this.viewPortWidth =
+      this.props.width +
+      2 * this.props.radius +
+      2 * this.universalPadding +
+      this.textMargin;
+    this.initailContentOffset = this.props.active
+      ? 0
+      : this.props.width + this.textMargin;
   }
 
   async componentDidMount() {
-    if (!this.props.active && Platform.OS === 'android') {
+    if (!this.props.active && Platform.OS === "android") {
       // Android hack to push scroll view to end at the initial rendering...
       this.intervalRef = setInterval(() => {
         if (this.scrollRef) {
@@ -35,17 +41,20 @@ class ToggleSwitch extends React.Component {
     }
   }
 
-  setScrollViewRef = (ref) => this.scrollRef = ref
+  setScrollViewRef = (ref) => (this.scrollRef = ref);
 
-  setTouchableRef = (ref) => this.touchableOpacity = ref
+  setTouchableRef = (ref) => (this.touchableOpacity = ref);
 
   updateState = (active) => {
-    this.setState({
-      isActive: active
-    }, () => {
-      this.props.onValueChange(active);
-    });
-  }
+    this.setState(
+      {
+        isActive: active,
+      },
+      () => {
+        this.props.onValueChange(active);
+      }
+    );
+  };
 
   toggleSwitch = () => {
     const { isActive } = this.state;
@@ -53,139 +62,149 @@ class ToggleSwitch extends React.Component {
       this.scrollRef.scrollToEnd();
       this.updateState(false);
     } else {
-      this.scrollRef.scrollTo({x: 0, y: 0, animated: true})
+      this.scrollRef.scrollTo({ x: 0, y: 0, animated: true });
       this.updateState(true);
     }
-  }
+  };
 
   onDragEnd = (e) => {
     const { contentOffset } = e.nativeEvent;
-    if(contentOffset.x > (this.props.width ) / 2) {
+    if (contentOffset.x > this.props.width / 2) {
       this.scrollRef.scrollToEnd();
       this.updateState(false);
     } else {
-      this.scrollRef.scrollTo({x: 0, y: 0, animated: true})
+      this.scrollRef.scrollTo({ x: 0, y: 0, animated: true });
       this.updateState(true);
     }
     this.touchableOpacity.setOpacityTo(1, 300);
-  }
+  };
 
   onDragStart = (e) => {
     this.touchableOpacity.setOpacityTo(0.5, 300);
-  }
+  };
 
   render() {
-    const { text: { on = 'ON', off = 'OFF', activeTextColor, inactiveTextColor },
-     color: { active, inactive, indicator, activeBorder, inactiveBorder }, 
-     textStyle = {},
-     disabled = false
+    const {
+      text: { on = "ON", off = "OFF", activeTextColor, inactiveTextColor },
+      color: { active, inactive, indicator, activeBorder, inactiveBorder },
+      textStyle = {},
+      disabled = false,
     } = this.props;
     const { isActive } = this.state;
-    
+
     return (
-      <TouchableOpacity  onPress={console.log("Onpressed")} activeOpacity={1} ref = {this.setTouchableRef} disabled={disabled}>
-      <View
-        style={[
-          styles.viewPort,
-          { 
-            width: this.viewPortWidth,
-            height: this.props.radius * 2 +  this.universalPadding * 2,
-            opacity: 1,
-            borderRadius: this.viewPortRadius,
-            borderWidth: this.borderWidth,
-            borderColor: isActive ? activeBorder : inactiveBorder,
-            backgroundColor: isActive? active: inactive
-          }
-        ]}
+      <TouchableOpacity
+        onPress={() => {}}
+        activeOpacity={1}
+        ref={this.setTouchableRef}
+        disabled={disabled}
       >
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          ref={this.setScrollViewRef}
-          onScrollEndDrag={this.onDragEnd}
-          onScrollBeginDrag={this.onDragStart}
-          scrollEnabled={!disabled}
-          scrollsToTop={false}
-          contentOffset={{x: this.initailContentOffset, y: 0}}
-          style={{ width: this.viewPortWidth }}
+        <View
+          style={[
+            styles.viewPort,
+            {
+              width: this.viewPortWidth,
+              height: this.props.radius * 2 + this.universalPadding * 2,
+              opacity: 1,
+              borderRadius: this.viewPortRadius,
+              borderWidth: this.borderWidth,
+              borderColor: isActive ? activeBorder : inactiveBorder,
+              backgroundColor: isActive ? active : inactive,
+            },
+          ]}
         >
-          <View
-            style={[
-              styles.container,
-              { 
-                opacity: 1,
-                backgroundColor: isActive? active: inactive,
-                height:  this.props.radius * 2 +  this.universalPadding * 2 
-              }
-            ]}
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            ref={this.setScrollViewRef}
+            onScrollEndDrag={this.onDragEnd}
+            onScrollBeginDrag={this.onDragStart}
+            scrollEnabled={!disabled}
+            scrollsToTop={false}
+            contentOffset={{ x: this.initailContentOffset, y: 0 }}
+            style={{ width: this.viewPortWidth }}
           >
             <View
               style={[
-                styles.activeView,
-                { 
-                  width: this.props.width,
-                  // marginLeft: this.viewPortRadius
-                }
+                styles.container,
+                {
+                  opacity: 1,
+                  backgroundColor: isActive ? active : inactive,
+                  height: this.props.radius * 2 + this.universalPadding * 2,
+                },
               ]}
             >
-              <Text 
-                style={[
-                  {
-                    alignSelf: 'center', 
-                    textAlign: 'center',
-                    color: isActive ? activeTextColor : inactiveTextColor
-                  },
-                  textStyle
-                ]}
-              >
-                {on}
-              </Text>
-            </View>
-            <TouchableWithoutFeedback onPress={this.toggleSwitch} disabled={disabled}>
               <View
                 style={[
-                  styles.indicatorWrapper,
+                  styles.activeView,
                   {
-                    justifyContent: isActive ? 'flex-end' : 'flex-start',
-                    padding: this.universalPadding 
-                  }
-                ]}>
+                    width: this.props.width,
+                    // marginLeft: this.viewPortRadius
+                  },
+                ]}
+              >
+                <Text
+                  style={[
+                    {
+                      alignSelf: "center",
+                      textAlign: "center",
+                      color: isActive ? activeTextColor : inactiveTextColor,
+                    },
+                    textStyle,
+                  ]}
+                >
+                  {on}
+                </Text>
+              </View>
+              <TouchableWithoutFeedback onPress={() => {}} disabled={disabled}>
                 <View
                   style={[
-                    styles.indicator,
-                    { backgroundColor: indicator, 
-                      borderColor: isActive ? active : inactive,
-                      width: this.props.radius * 2,
-                      height: this.props.radius * 2,
-                      borderRadius: this.props.radius
-                    }
+                    styles.indicatorWrapper,
+                    {
+                      justifyContent: isActive ? "flex-end" : "flex-start",
+                      padding: this.universalPadding,
+                    },
                   ]}
-                />
-              </View>
-            </TouchableWithoutFeedback>
-            <View
-              style={[
-                styles.inactiveView,
-                { width: this.props.width,
-                  // marginRight: this.viewPortRadius
-                }
-              ]}
-            >
-              <Text
+                >
+                  <View
+                    style={[
+                      styles.indicator,
+                      {
+                        backgroundColor: indicator,
+                        borderColor: isActive ? active : inactive,
+                        width: this.props.radius * 5,
+                        height: this.props.radius * 5,
+                        borderRadius: this.props.radius,
+                      },
+                    ]}
+                  />
+                </View>
+              </TouchableWithoutFeedback>
+              <View
                 style={[
-                {
-                  alignSelf: 'center', 
-                  textAlign: 'center',
-                  color: isActive ? activeTextColor : inactiveTextColor
-                },
-                textStyle
-              ]}>
-                {off}
-              </Text>
+                  styles.inactiveView,
+                  {
+                    width: this.props.width,
+                    // marginRight: this.viewPortRadius
+                  },
+                ]}
+              >
+                <Text
+                  style={[
+                    {
+                      alignSelf: "center",
+                      textAlign: "center",
+                      color: isActive ? activeTextColor : inactiveTextColor,
+                    },
+                    textStyle,
+                  ]}
+                >
+                  {off}
+                </Text>
+              </View>
             </View>
-          </View>
-        </ScrollView>
-      </View>
+          </ScrollView>
+        </View>
       </TouchableOpacity>
     );
   }
@@ -195,40 +214,40 @@ const styles = StyleSheet.create({
   viewPort: {
     paddingTop: 0,
     paddingBottom: 0,
-    backgroundColor: 'transparent',
-    overflow: 'hidden',
-    alignItems: 'center'
+    backgroundColor: "transparent",
+    overflow: "hidden",
+    alignItems: "center",
   },
   container: {
-    position: 'relative',
+    position: "relative",
     padding: 0,
-    flexDirection: 'row',
-    alignSelf: 'center'
+    flexDirection: "row",
+    alignSelf: "center",
   },
-  indicatorWrapper:{
-    flexDirection: 'row',
-    alignItems: 'center'
+  indicatorWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
   },
   indicator: {
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.5,
     shadowRadius: 2,
-    elevation: 5
+    elevation: 5,
   },
   activeView: {
     marginRight: 3,
     marginLeft: 5,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center'
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
   inactiveView: {
     marginLeft: 3,
     marginRight: 5,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
 export default ToggleSwitch;
